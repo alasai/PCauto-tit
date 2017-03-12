77e9034a2b5846a4ccf189758e7b974c435730b2
binyuli <1164835947@qq.com>
2017-03-13 05:32:26 +0800

    modify contact spider

--- PCauto/spiders/PCauto_dealer_contact.py
+++ PCauto/spiders/PCauto_dealer_contact.py
@@ -17,14 +17,28 @@ class PCautoDealerContactSpider(RedisSpider):
         contact_urls = mongoservice.get_dealer_contact()
         for url in contact_urls:
             yield Request(url, dont_filter=True, callback=self.get_contacts)
+            # yield Request(url, dont_filter=True,headers={
+            #     'Host': "price.pcauto.com.cn",
+            #     'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            #     # 'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0",
+            #     'User-Agent': "Mozilla / 5.0(Windows NT 6.1;WOW64) AppleWebKit / 537.36(KHTML, likeGecko) Chrome / 56.0.2924.87Safari / 537.36",
+            #     'Accept-Language': "zh-CN,zh;q=0.8",
+            #     'Accept-Encoding': "gzip, deflate, sdch",
+            #     'Connection': "keep-alive",
+            #     'Cache-Control': "max-age=0",
+            #     'Upgrade-Insecure-Requests': "1",
+            #     'Referer': "http: // price.pcauto.com.cn / 89452 / news.html"
+            # }, callback=self.get_contacts)
             yield Request(url, callback=self.get_url)
 
     def get_contacts(self, response):
         soup = BeautifulSoup(response.body_as_unicode(), 'lxml')
-        tabs = soup.find('div', class_="ctltag").find_all('a')
-        for tab in tabs[1:]:
-            href = tab.get('href')
-            yield Request(href, callback=self.get_url)
+        tab_list = soup.find('div', class_="ctltag")
+        if tab_list:
+            tabs = tab_list.find_all('a')
+            for tab in tabs[1:]:
+                href = tab.get('href')
+                yield Request(href, callback=self.get_url)
 
     def get_url(self,response):
         soup = BeautifulSoup(response.body_as_unicode(), 'lxml')
