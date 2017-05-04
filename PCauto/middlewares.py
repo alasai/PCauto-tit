eb93490c11067f227d5bf4e1aae7ca356d1cea3f
binyuli <1164835947@qq.com>
2017-05-04 19:17:07 +0800

    add proxy for usedcar spider

--- PCauto/middlewares.py
+++ PCauto/middlewares.py
@@ -28,3 +28,33 @@ class RotateUserAgentMiddleware(object):
             logger.info('Crawled (%d) <GET %s>' % (response.status,response.url))
         return response
 
+
+class ProxyMiddleware(object):
+    """ 使用代理IP """
+    proxies = list()
+    def process_request(self, request, spider):
+        if self.proxies:
+            proxy = random.choice(self.proxies)
+        else:
+            self.get_proxies()
+            proxy = random.choice(self.proxies)
+        request.meta['proxy'] = "http://%s" % proxy
+        # print request.meta['proxy']
+        """ 如若使用的代理ip需要验证,再一句需求修改此处代码
+            encoded_user_pass = base64.encodestring(proxy['user_pass'])
+            request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
+        else:
+            request.meta['proxy'] = "http://%s" % proxy['ip_port']"""
+    def process_exception(self, request, exception, spider):
+        proxy = request.meta['proxy']
+        try:
+            print "remove the old proxy: " + proxy
+            self.proxies.remove(proxy)
+        except ValueError:
+            pass
+    def get_proxies(self):
+        """更新代理ip"""
+        self.proxies = ['122.5.128.110:808']
+        """
+        加入更新代理ip的方法
+        """
